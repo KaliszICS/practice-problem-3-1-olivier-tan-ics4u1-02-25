0fd173307e712a83640646f3f4368ab8a89d9260
olivier-tan-ics4u1-02-25 <olivier.tan@student.tdsb.on.ca>
2025-04-01 13:58:42 +0000

    update

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,6 +4,29 @@ public class PracticeProblem {
 
 	}
 
-	
+	public static int sumOfDigits(int x){
+
+		if (x==0){
+			return 0;
+		}
+
+		if (x < 0){
+			x *= -1;
+		}
+
+		return (x % 10) + sumOfDigits(x /= 10);
+	}
+
+	public static int factorial(int n){
+
+		if (n == 0){
+			return 1;
+	}		
+		if (n < 0 ){
+			return 0;
+		}	
+
+		return factorial(n-1)*(n);
+}
 
 }
